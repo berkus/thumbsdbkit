593e55fdf5a8dbfb59cc9df0055ccbf4ba359177
Berkus Decker <berkus+github@metta.systems>
2024-10-07 02:45:59 +0300

    feat: ✨ Replace clap with argh

--- src/kit.rs
+++ src/kit.rs
@@ -16,55 +16,49 @@
 
 use crate::error::Error;
 use crate::format::Formatter;
-use clap::ArgMatches;
 use std::fs;
 use std::io::Write;
-use std::path::Path;
 use thumbsdb::ThumbsDb;
 
-pub(crate) fn kit(all_matches: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
-    // ls
-    if let Some(matches) = all_matches.subcommand_matches("ls") {
-        let filepath = matches.value_of("FILE").unwrap();
-        let details = matches.is_present("details");
-        let idirid = matches.is_present("idirid");
-        let color = matches.is_present("color");
+pub(crate) fn ls(ls: &crate::SubCommandLs) -> Result<(), Box<dyn std::error::Error>> {
+    let db = ThumbsDb::from_path(&ls.file)?;
+    let formatter = Formatter {
+        details: ls.details,
+        idirid: ls.idirid,
+        color: ls.color,
+    };
+    let mut n = 0usize;
+    for thumbnail in db.iterate() {
+        formatter.print_thumbnail(thumbnail);
+        n += 1;
+    }
+    formatter.print_suffix(n);
+    Ok(())
+}
 
-        let db = ThumbsDb::from_path(filepath)?;
-        let formatter = Formatter {
-            details,
-            idirid,
-            color,
-        };
-        let mut n = 0usize;
-        for thumbnail in db.iterate() {
-            formatter.print_thumbnail(thumbnail);
-            n += 1;
-        }
-        formatter.print_suffix(n);
-    } else if let Some(matches) = all_matches.subcommand_matches("extract") {
-        let filepath = matches.value_of("FILE").unwrap();
-        let outdirpath = Path::new(matches.value_of("OUTDIR").unwrap());
-        if !outdirpath.exists() {
-            fs::create_dir(outdirpath)?;
-        } else if !outdirpath.is_dir() {
-            Err(Error::OutDirNotDir)?;
-        }
-        let db = ThumbsDb::from_path(filepath)?;
-        for thumbnail in db.iterate() {
-            let outfilepath = outdirpath.join(thumbnail.name());
-            if outfilepath.exists() && !outfilepath.is_file() {
-                eprintln!(
-                    "warning: {:?} exists and isn't a file. Skipping",
-                    &outfilepath
-                );
-            } else {
-                let mut f = fs::File::create(&outfilepath)?;
-                let mut buffer = Vec::<u8>::new();
-                db.extract_thumbnail(thumbnail, &mut buffer)?;
-                f.write_all(&buffer)?;
-                println!("{:<20} extracted to {:?}", thumbnail.name(), &outfilepath);
-            }
+pub(crate) fn extract(
+    extract: &crate::SubCommandExtract,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let outdirpath = extract.outdir.as_path();
+    if !outdirpath.exists() {
+        fs::create_dir(outdirpath)?;
+    } else if !outdirpath.is_dir() {
+        Err(Error::OutDirNotDir)?;
+    }
+    let db = ThumbsDb::from_path(&extract.file)?;
+    for thumbnail in db.iterate() {
+        let outfilepath = outdirpath.join(thumbnail.name());
+        if outfilepath.exists() && !outfilepath.is_file() {
+            eprintln!(
+                "warning: {:?} exists and isn't a file. Skipping",
+                &outfilepath
+            );
+        } else {
+            let mut f = fs::File::create(&outfilepath)?;
+            let mut buffer = Vec::<u8>::new();
+            db.extract_thumbnail(thumbnail, &mut buffer)?;
+            f.write_all(&buffer)?;
+            println!("{:<20} extracted to {:?}", thumbnail.name(), &outfilepath);
         }
     }
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -14,69 +14,64 @@
 //
 // Author: zadig <thomas chr(0x40) bailleux.me>
 
-use clap::{App, Arg, SubCommand};
+use argh::FromArgs;
+use std::path::PathBuf;
 
 mod error;
 mod format;
 mod kit;
 
+#[derive(FromArgs, PartialEq, Debug)]
+/// ThumbsDBkit.
+struct TopLevel {
+    #[argh(subcommand)]
+    nested: SubCommandEnum,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum SubCommandEnum {
+    Ls(SubCommandLs),
+    Extract(SubCommandExtract),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// List thumbnails.
+#[argh(subcommand, name = "ls")]
+pub(crate) struct SubCommandLs {
+    /// colorize the output
+    #[argh(switch, short = 'c')]
+    color: bool,
+    /// print more details for each entry
+    #[argh(switch, short = 'd')]
+    details: bool,
+    /// print the index number of each file
+    #[argh(switch, short = 'i')]
+    idirid: bool,
+    /// thumbs.db to analyze
+    #[argh(positional)]
+    file: String,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Extract thumbnails.
+#[argh(subcommand, name = "extract")]
+pub(crate) struct SubCommandExtract {
+    /// output directory where extracted thumbnails will be stored
+    #[argh(option, short = 'o', default = "PathBuf::from(\".\")")]
+    outdir: PathBuf,
+    /// thumbs.db to analyze
+    #[argh(positional)]
+    file: String,
+}
+
 fn main() {
-    let matches = App::new("ThumbsDBkit")
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .subcommand(
-            SubCommand::with_name("ls")
-                .about("List thumbnails")
-                .arg(
-                    Arg::with_name("FILE")
-                        .help("Thumbs.db to analyze")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("color")
-                        .long("color")
-                        .required(false)
-                        .takes_value(false)
-                        .help("colorize the output"),
-                )
-                .arg(
-                    Arg::with_name("details")
-                        .short("d")
-                        .long("details")
-                        .required(false)
-                        .takes_value(false)
-                        .help("print more details for each entry"),
-                )
-                .arg(
-                    Arg::with_name("idirid")
-                        .short("i")
-                        .long("idirid")
-                        .required(false)
-                        .takes_value(false)
-                        .help("print the index number of each file"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("extract")
-                .about("Extract thumbnails")
-                .arg(
-                    Arg::with_name("FILE")
-                        .help("Thumbs.db to analyze")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("OUTDIR")
-                        .short("o")
-                        .long("out")
-                        .help("Output directory where extracted thumbnails will be stored")
-                        .required(true)
-                        .takes_value(true),
-                ),
-        )
-        .get_matches();
-    match kit::kit(&matches) {
+    let args: TopLevel = argh::from_env();
+
+    match match args.nested {
+        SubCommandEnum::Ls(ls) => kit::ls(&ls),
+        SubCommandEnum::Extract(extract) => kit::extract(&extract),
+    } {
         Err(e) => eprintln!("An error has occured: {}", e),
         _ => {}
     }
